1baef4a7eb0d23453071e13365fc1b8105410f3b
Golden Ogbeka <ogbekagolden@gmail.com>
2020-11-03 19:20:53 +0100

    Initial commit

---
+++ Test/app/src/main/java/test/com/nedlog/test/TestStart.java
@@ -0,0 +1,13 @@
+package test.com.nedlog.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestStart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_start);
+    }
+}
